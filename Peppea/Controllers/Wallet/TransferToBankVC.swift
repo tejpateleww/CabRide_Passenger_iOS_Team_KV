de54a24393bf8910d854b55f0512f659b074a867
EWW80 <>
2019-07-11 10:44:16 +0530

    Ui: Transfer To Bank VC Wallet Vc

---
+++ Peppea/Controllers/Wallet/TransferToBankVC.swift
@@ -0,0 +1,30 @@
+//
+//  TransferToBankVC.swift
+//  Peppea
+//
+//  Created by EWW80 on 11/07/19.
+//  Copyright © 2019 Mayur iMac. All rights reserved.
+//
+
+import UIKit
+
+class TransferToBankVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Peppea/Controllers/Wallet/WalletViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WalletViewController.swift
+//  Peppea
+//
+//  Created by EWW80 on 10/07/19.
+//  Copyright © 2019 Mayur iMac. All rights reserved.
+//
+
+import UIKit
+
+class WalletViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func btnSendMoneyTapped(_ sender: Any) {
+    }
+    
+   
+    @IBAction func btnReceiveMoney(_ sender: Any) {
+    }
+    
+    @IBAction func btnTransferToBankTapped(_ sender: Any) {
+    }
+    @IBAction func btnHistoryTapped(_ sender: Any) {
+    }
+    @IBAction func btnTopUpTapped(_ sender: Any) {
+    }
+}
